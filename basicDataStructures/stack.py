435291efdb004eb2a5c3bc6f2641a616d4545265
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-01-14 07:56:35 -0500

    First implementation of a Stack class

---
+++ basicDataStructures/stack.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 14 07:46:44 2015
+
+@author: umesh
+"""
+
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def push(self,newItem):
+        self.items.append(newItem)
+        
+    def pop():
+        return(self.items.pop())
+    
+    def peek():
+        return(self.items[-1])
+    
+    def isEmpty():
+        stackLen = len(self.items)
+        return stackLen == 0
+    
+    def size():
+        return len(self.items)

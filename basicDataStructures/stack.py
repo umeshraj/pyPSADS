c68738c5d9df521731b7ea5d5dfaf723c47484f5
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-01-14 08:33:05 -0500

    minor cosmetic/PEP-8 change

--- basicDataStructures/stack.py
+++ basicDataStructures/stack.py
@@ -5,21 +5,36 @@ Created on Wed Jan 14 07:46:44 2015
 @author: umesh
 """
 
+
 class Stack:
     def __init__(self):
         self.items = []
-    
-    def push(self,newItem):
+
+    def push(self, newItem):
         self.items.append(newItem)
-        
-    def pop():
+
+    def pop(self):
         return(self.items.pop())
-    
-    def peek():
+
+    def peek(self):
         return(self.items[-1])
-    
-    def isEmpty():
+
+    def isEmpty(self):
         return self.items == []
-    
-    def size():
+
+    def size(self):
         return len(self.items)
+
+
+s = Stack()
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())

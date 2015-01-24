03b70c3d5471565f3bcf2e4734d3541a7640575f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-01-24 08:33:48 -0500

    Added option to show the coins selected for making change

--- Recursion/dynProgMakeChange.py
+++ Recursion/dynProgMakeChange.py
@@ -15,8 +15,40 @@ def dpMakeChange(coinValueList, change, minCoins):
    return minCoins[change]
    
 
-amnt = 23
-cList = [1,5,10,21,25]
+def dpMakeChangeTrkCoins(coinValueList, change, minCoins, coinsUsed):
+    """ changing dyn program to track coins used"""
+    for cents in range(change+1):
+        coinCount = cents
+        newCoin = 1
+        for j in [c for c in coinValueList if c <= cents]:
+            if minCoins[cents-j] + 1 < coinCount:
+                coinCount = minCoins[cents-j]+1
+                newCoin = j
+        minCoins[cents] = coinCount
+        coinsUsed[cents] = newCoin
+    return minCoins[change]
+   
+def printCoins(coinsUsed, amt):
+    """ print list of coins used to make amt"""
+    usedList = []
+    while amt > 0:
+        coin = coinsUsed[amt]
+        print coin
+        #usedList.append(coin)
+        amt = amt - coin
+    #return ",".split(usedList)
+   
+def main():
+    amnt = 20
+    clist = [1,5,10,21,25]
+    coinsUsed = [0]*(amnt+1)
+    coinCount = [0]*(amnt+1)
 
-coinsUsed = [0]*(amnt+1)
-print(dpMakeChange(cList, amnt, coinsUsed))
+    print("Making change for",amnt,"requires")
+    print(dpMakeChangeTrkCoins(clist,amnt,coinCount,coinsUsed),"coins")
+    print("They are:")
+    printCoins(coinsUsed,amnt)
+    print("The used list is as follows:")
+    print(coinsUsed)
+
+main()

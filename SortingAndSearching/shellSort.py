19c8d34bc2f683c8b2f359f3428ea7a1ce21e096
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2015-01-27 20:38:10 -0500

    Modified simple insertion sort to gapInsertion sort Added shellSort function

--- SortingAndSearching/shellSort.py
+++ SortingAndSearching/shellSort.py
@@ -5,22 +5,33 @@ Created on Tue Jan 27 19:59:14 2015
 @author: umesh
 """
 
+def shellSort(aList):
+    """ main function for shell sorting"""
+    subListLen = len(aList)//2
+    while subListLen > 0:
+        for startIdx in range(subListLen):
+            gapInsertionSort(aList, startIdx, subListLen)
+        print aList
+        subListLen = subListLen//2
+    
+
 def gapInsertionSort(aList, stIdx, gap):
     """ gap insertion sort for shell sort"""
     
     N = len(aList)
-    for outIdx in range(1, N):
+    for outIdx in range(stIdx+gap, N, gap):
         curPos = outIdx
         curVal = aList[curPos]
-        while (curPos >= 1) and (aList[curPos-1] > curVal):
-            aList[curPos] = aList[curPos-1]
-            curPos = curPos - 1
+        while (curPos >= gap) and (aList[curPos-gap] > curVal):
+            aList[curPos] = aList[curPos-gap]
+            curPos = curPos - gap
         aList[curPos] = curVal
     
     return aList
     
 alist = [54,26,93,17,77,31,44,55,20]
-gapInsertionSort(alist, 0, 1)
+#gapInsertionSort(alist, 0, 3)
+shellSort(alist)
 print(alist)
 
     

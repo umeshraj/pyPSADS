9943f03431d5caee0ebae4d8b6078776e25df8f7
umesh rajashekar <umesh.rajashekar@gmail.com>
2017-03-08 13:13:26 -0500

    Added parent LogicGate

--- Introduction/oop-LogicGates.py
+++ Introduction/oop-LogicGates.py
@@ -7,3 +7,14 @@ URL: https://goo.gl/AEpgFw
 @author: umesh
 """
 
+class LogicGate:
+    def __init__(self, gate_label):
+        self.label = gate_labe
+        self.output = None
+
+    def getLabel(self):
+        return self.label()
+
+    def getOutput(self):
+        self.output = self.performGateLogic()
+        return self.output

3303a778983300e8196bb094dbd5de0e6dd043ee
umesh rajashekar <umesh.rajashekar@gmail.com>
2017-03-08 08:54:09 -0500

    Simple version of adding two fractions

--- Introduction/oop-Fraction.py
+++ Introduction/oop-Fraction.py
@@ -17,7 +17,18 @@ class Fraction:
         out_str = "{0}/{1}".format(self.num, self.den)
         return out_str
 
+    def __add__(self, new_frac):
+        new_num = self.num*new_frac.den + self.den*new_frac.num
+        new_den = self.den * new_frac.den
+        return Fraction(new_num, new_den)
+
+# testing the show/print functions
 myFrac = Fraction(3, 4)
 myFrac.show()
 print("I ate {0} of my pizza".format(myFrac))
 
+# adding fractions
+f1=Fraction(1,4)
+f2=Fraction(1,2)
+f3=f1+f2
+print(f3)

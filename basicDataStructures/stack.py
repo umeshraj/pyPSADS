b6a7ece40cb17f74e495aed673b08156ce45a087
umesh rajashekar <umesh.rajashekar@gmail.com>
2017-03-09 08:51:49 -0500

    Reimplemented Stack ADT

--- basicDataStructures/stack.py
+++ basicDataStructures/stack.py
@@ -5,4 +5,37 @@ Created on Wed Jan 14 07:46:44 2015
 @author: umesh
 """
 
+class Stack:
+    """ A class for the Stack abstract data type"""
+    def __init__(self):
+        self.items = []
 
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+
+s=Stack()
+
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
